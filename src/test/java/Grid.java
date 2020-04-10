6f38b306a2057b1d41db1c909ac5390de8a72453
yuer <yuer@RED_LINE.fios-router.home>
2020-04-10 10:17:29 -0400

    SMB-GitProject

---
+++ src/test/java/Grid.java
@@ -0,0 +1,15 @@
+
+public class Grid {
+
+	public static void GridTest() {
+	
+	System.setProperty("webdriver.chrome.driver",
+			System.getProperty("user.dir") 
+			+ "\\src\\test\\resources\\Executeable\\chromedriver.exe");
+
+
+
+
+
+	}
+}
